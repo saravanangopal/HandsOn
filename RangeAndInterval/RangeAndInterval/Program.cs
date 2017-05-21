251ea9c87cb3f700e37a53151aa6c96017604d03
Saravanan Gopal <mailsaravanangopal@gmail.com>
2017-05-21 19:12:39 +0530

    Initial commit on RangeAndInterval project and Array Sum

---
+++ RangeAndInterval/RangeAndInterval/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Enter Starting number for a range between 1 to 1000");
+                int minimum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Enter Ending number for a range between 1 to 1000");
+                int maximum = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Provide an interval for cumulative calculation");
+                int interval = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Choose an arithmetic operation");
+                Console.WriteLine("1 for Addtion");
+                Console.WriteLine("2 for Substraction");
+                Console.WriteLine("3 for Multiplication");
+                Console.WriteLine("4 for Division");
+                ArithmeticOperators arithmeticOperator = (ArithmeticOperators)(Convert.ToInt32(Console.ReadLine()));
+                IntervalCalc(minimum, maximum, interval, arithmeticOperator);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+                Console.ReadLine();
+            }
+        }
+        public static void IntervalCalc(int minimum, int maximum, int interval, ArithmeticOperators operators)
+        {
+            //Check Starting and Ending Number are with in range
+            IsWithinRange(minimum, true);
+            IsWithinRange(maximum, false);
+
+            //Other mandatory validations
+            Validations(minimum, maximum, interval);
+
+            //Build the list of numbers to perform arithmetic operations
+            List<double> numbers = GetNumbersToCalculate(minimum, maximum, interval);
+
+            //Perform arithemetic operation
+            double result = GetComputedValue(operators, numbers);
+
+            Console.WriteLine("The result is " + result);
+            Console.ReadLine();
+        }
+        private static void Validations(int minimum, int maximum, int interval)
+        {
+            //Starting and Ending number should not be same
+            if (minimum == maximum)
+            {
+                throw new ArgumentOutOfRangeException("Starting and Ending number can not be same");
+            }
+
+            if (interval < minimum || interval > maximum)
+            {
+                throw new ArgumentOutOfRangeException("Interva should be bewtween Starting and Ending number");
+            }
+
+            //The Ending should be greater than sum of starting and interval Value
+            if ((minimum + interval) > maximum)
+            {
+                throw new ArgumentOutOfRangeException("Sum of Starting number and Interval should not exceed ending number");
+            }
+        }
+        private static double GetComputedValue(ArithmeticOperators operators, List<double> numbers)
+        {
+            Double result = 0;
+            switch (operators)
+            {
+                case ArithmeticOperators.Addition:
+                    result = numbers.Aggregate((a, x) => a + x);
+                    break;
+                case ArithmeticOperators.Substraction:
+                    result = numbers.Aggregate((a, x) => a - x);
+                    break;
+                case ArithmeticOperators.Multiplication:
+                    result = numbers.Aggregate((a, x) => a * x);
+                    break;
+                case ArithmeticOperators.Division:
+                    result = numbers.Aggregate((a, x) => a / x);
+                    break;
+                default:
+                    break;
+            }
+
+            return result;
+        }
+        private static List<double> GetNumbersToCalculate(int minimum, int maximum, int interval)
+        {
+            List<Double> numbers = new List<Double>();
+            Double iterationValue = minimum;
+            for (Double i = iterationValue; iterationValue < maximum; iterationValue++)
+            {
+                Double valueToCalculate = iterationValue + interval - 1;
+                numbers.Add(valueToCalculate);
+                iterationValue = valueToCalculate;
+            }
+
+            return numbers;
+        }
+        public static void IsWithinRange(int value, bool IsMinimum)
+        {
+            if (value < 1 || value > 1000)
+            {
+                if (IsMinimum)
+                {
+                    throw new ArgumentOutOfRangeException("Minimum value should be between 1 and 1000");
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("Maximum value should be between 1 and 1000");
+                }
+            }
+        }
+        public enum ArithmeticOperators
+        {
+            Addition = 1,
+            Substraction = 2,
+            Multiplication = 3,
+            Division = 4
+        }
+    }
+}
---
+++ TryArraySum/TryArraySum/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Initialize different categories of array with value
+            Object[] values = new Object[4] { 2, 5, "X", 3 };
+            Object[][] jaggedValues = new Object[4][]
+            {
+                    new Object[3] {5,2,2},
+                    new Object[3] {5,"X",4},
+                    new Object[3] {8,4,3},
+                    new Object[3] {2,5,4},
+            };
+
+            Object[][] rectangularArray = new Object[6][]
+           {
+                    new Object[1] {1},
+                    new Object[4] {1,2,5,3},
+                    new Object[3] {"X",8,9},
+                    new Object[2] {2,8},
+                    new Object[3] {2,4,7},
+                    new Object[1] {9}
+           };
+
+           SumArray(values, jaggedValues, rectangularArray);
+
+        }
+
+        public static void SumArray(object[] singleArray = null, object[][] jaggedArray = null, object[][] rectangularArray = null)
+        {
+            Int32 singleArraySum = 0;
+            Int32 jaggedArraySum = 0;
+            Int32 rectangularArraySum = 0;
+
+            singleArraySum = GetSingleArraySum(singleArray, singleArraySum);
+            jaggedArraySum = GetJaggedArraySum(jaggedArray, jaggedArraySum);
+            rectangularArraySum = GetRectangularSumArray(rectangularArray, rectangularArraySum);
+
+            Console.WriteLine("Single Array Sum = " + singleArraySum);
+            Console.WriteLine("Jagged Array Sum = " + jaggedArraySum);
+            Console.WriteLine("Rectangular Array Sum = " + rectangularArraySum);
+        }
+
+        private static int GetRectangularSumArray(object[][] rectangularArray, int rectangularArrayResult)
+        {
+            if (rectangularArray != null)
+            {
+                for (int i = 0; i < rectangularArray.Length; i++)
+                {
+                    for (int j = 0; j < rectangularArray[i].Length; j++)
+                    {
+                        //Use try catch to skip the conversion issue for non numeric value
+                        try
+                        {
+                            rectangularArrayResult = rectangularArrayResult + Convert.ToInt32(rectangularArray[i][j]);
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+            }
+
+            return rectangularArrayResult;
+        }
+
+        private static int GetJaggedArraySum(object[][] jaggedArray, int jaggedArrayResult)
+        {
+            if (jaggedArray != null)
+            {
+                for (int i = 0; i < jaggedArray.Length; i++)
+                {
+                    for (int j = 0; j < jaggedArray[i].Length; j++)
+                    {
+                        //Use try catch to skip the conversion issue for non numeric value
+                        try
+                        {
+                            jaggedArrayResult = jaggedArrayResult + Convert.ToInt32(jaggedArray[i][j]);
+                        }
+                        catch
+                        {
+
+                        }
+                    }
+                }
+            }
+
+            return jaggedArrayResult;
+        }
+
+        private static int GetSingleArraySum(object[] singleArray, int result)
+        {
+            if (singleArray != null)
+            {
+                foreach (object item in singleArray)
+                {
+                    //Use try catch to skip the conversion issue for non numeric value
+                    try
+                    {
+                        result = result + Convert.ToInt32(item);
+                    }
+                    catch
+                    {
+
+                    }
+
+                }
+            }
+
+            return result;
+        }
+    }
+}
